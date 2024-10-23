d3aef7c702ba26c8ead07967328a7550699888db
Czz <Eason0208@163.com>
2024-10-23 21:59:57 +0800

    添加计算

--- Mandelbrot/calc.cpp
+++ Mandelbrot/calc.cpp
@@ -6,9 +6,11 @@ int inSet(double c_r, double c_i) {
 	double z_r = 0, z_i = 0;
 
 	for (int i = 1; i <= MAX_ITER; i++) {
-		z_r = SQ(z_r) - SQ(z_i) + c_r;
-		z_i = 2 * z_r * z_i + c_i;
-		if (SQ(z_r) + SQ(z_i) > 4) return i;
+		double next_r = SQ(z_r) - SQ(z_i) + c_r;
+		double next_i = 2 * z_r * z_i + c_i;
+		z_r = next_r;
+		z_i = next_i;
+		if (SQ(z_r) + SQ(z_i) > 10000) return i;
 	}
 	return 0;
 }
--- Mandelbrot/main.cpp
+++ Mandelbrot/main.cpp
@@ -1,12 +1,31 @@
 #include <easyx.h>
 #include <stdlib.h>
 
-#define MAX_WIDTH 1920
-#define MAX_HEIGHT 1080
+#include "calc.h"
+
+#define MAX_WIDTH 2560
+#define MAX_HEIGHT 2560
+
+#define MAX_R_NUM 0.6
+#define MIN_R_NUM -2.0
+#define MAX_I_NUM 1.3
+#define MIN_I_NUM -1.3
+#define MAX_ITER 1024.0
 
 int main() {
 	IMAGE* MdbrS = new IMAGE(MAX_WIDTH, MAX_HEIGHT);
+	SetWorkingImage(MdbrS);
 
+	for (int x = 0; x < MAX_WIDTH; x++) {
+		for (int y = 0; y < MAX_HEIGHT; y++) {
+			double r = MIN_R_NUM + (MAX_R_NUM - MIN_R_NUM) * x / MAX_WIDTH;
+			double i = MAX_I_NUM - (MAX_I_NUM - MIN_I_NUM) * y / MAX_HEIGHT;
+			int ite_time = inSet(r, i);
+			putpixel(x, y, ((int(ite_time / MAX_ITER * 0xFFFFFF))));
+		}
+	}
+
+	saveimage(L"debug.png", MdbrS);
 	//绘制到绘图窗口，调试用
 	initgraph(MAX_WIDTH, MAX_HEIGHT);
 	putimage(0, 0, MdbrS);
